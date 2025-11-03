4d8b6630a83ace43abcfd973d0aca26245fc3f75
Aiswarya Prabhalan <Aiswarya.Prabhalan@fdmgroup.com>
2025-11-03 16:33:22 -0500

    Changed Runner

--- src/test/java/com/fdmgroup/stepdefinitions/CucumberRunner.java
+++ src/test/java/com/fdmgroup/stepdefinitions/CucumberRunner.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
+//Runner
 @RunWith(Cucumber.class)
 @CucumberOptions(
 	features = "src/test/resources/Feature",
